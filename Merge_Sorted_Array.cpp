2633ccabb2894ca2653befbd8097a2c2f45f5598
Mehadi Talha <mehaditalha62@gmail.com>
2026-03-01 05:15:16 +0600

    Week_01_Day_05

---
+++ Merge_Sorted_Array.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h> 
+using namespace std;
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i = 0, j = 0;
+        vector<int>temp;
+        while(i<m && j<n){
+            if(nums1[i] <= nums2[j]){
+                temp.push_back(nums1[i++]);
+            }else{
+                temp.push_back(nums2[j++]);
+            }
+        }
+
+        while(i<m){
+            temp.push_back(nums1[i++]);
+        }
+
+        while(j<n){
+            temp.push_back(nums2[j++]);
+        }
+
+        for(int i = 0, x = 0 ; i<nums1.size(); i++){
+            nums1[i] = temp[x++];
+        }
+    }
+};
