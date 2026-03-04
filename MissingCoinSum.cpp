1065dce67412b72a4cf52d388a346ec239842e08
Mehadi Talha <mehaditalha62@gmail.com>
2026-03-05 04:33:09 +0600

    Week02Day04

---
+++ MissingCoinSum.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    long long n, reach = 1;
+    cin>>n;
+    vector<long long>arr(n);
+    for(int i = 0; i<n; i++){
+        cin>>arr[i];
+    }
+    sort(arr.begin(), arr.end());
+    for(int i : arr){
+        if (i > reach){
+            break;
+        }
+        reach+=i;
+    }
+    cout<<reach<<endl;
+}
