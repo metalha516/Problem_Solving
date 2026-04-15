e550ad3bb5bc0813501ff7cbaedf090f6e4fc34a
Mehadi Talha <mehaditalha62@gmail.com>
2026-04-15 20:28:39 +0600

    Daily Practise implimentation

---
+++ is_rated.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    long long t;
+    cin>>t;
+    vector<pair<long long ,long long>>term;
+    string result;
+    while(t--){
+        long long a;
+        long long b;
+        cin>>a;
+        cin>>b;
+        term.push_back(make_pair(a, b));
+    }
+    // sort(term.begin(), term.end());
+    
+    for(long long i = 0; i<term.size(); i++){
+        if(abs(term[i].first - term[i].second) > 0){ cout<<"rated"; return 0;}
+    }
+    bool Decreasing = true;
+
+    for(long long i = 0; i < term.size()-1; i++){
+        if(term[i].first < term[i+1].first){
+            cout<<"unrated"; return 0;
+        }
+    }
+    cout<<"maybe"<<endl; return 0;
+}
