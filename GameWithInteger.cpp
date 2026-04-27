9705af8bfe0abbb4d365cb8143af253145322394
Mehadi Talha <mehaditalha62@gmail.com>
2026-04-27 21:23:21 +0600

    Daily Practise 12

---
+++ GameWithInteger.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// string solve(int n){
+//     bool vanya = false;
+//     for(int i = 0; i<10; i++){
+//         if((n+1 ))
+//     }
+// }
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        if(n%3 == 0){
+            cout<<"Second"<<endl;
+        }else{
+            cout<<"First"<<endl;
+        }
+    }
+}
