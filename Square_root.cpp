fab740517a95d1c96c14563efa74dd2844d34a54
Mehadi Talha <mehaditalha62@gmail.com>
2026-05-02 22:34:52 +0600

    GeekForGeeks Problem 07

---
+++ Square_root.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+    int floorSqrt(int n) {
+       
+        return (int) sqrt(n);
+        
+    }
+
+int main(){
+    int x;
+    cin>>x;
+
+    int res = sqrt(x);
+
+
+    cout<<res;
+}
