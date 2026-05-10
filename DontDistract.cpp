b10e6951bb395620a88c5b1d2b2b53f538cc5de3
Mehadi Talha <mehaditalha62@gmail.com>
2026-05-10 21:29:17 +0600

    Daily Practise 33

---
+++ DontDistract.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool solve(string x){
+    vector<bool>visited(26, false);
+    bool solution = false;
+    
+    for(int i = 0; i<x.size(); i++){
+        if(i>0 && x[i] == x[i-1]){
+            continue;
+        }
+
+        if(visited[x[i] - 'A']) return false;
+       
+        visited[x[i] - 'A'] = true;
+    }
+
+    return true;
+}
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        string x;
+        cin>>x;
+        cout<<(solve(x) ? "YES" : "NO")<<endl;
+    }
+}
