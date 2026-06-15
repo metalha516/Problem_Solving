3f13a5290a3e73bc12e86b5326a3a54a4e5e3d39
metalha516 <mehaditalha62@gmail.com>
2026-06-16 04:09:57 +0600

    Daily Practise 60

---
+++ KefaandFirstSteps_580A.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<long long> arr(n);
+
+    for(auto &x : arr){
+        cin >> x;
+    }
+
+    int ans = 1;
+    int cnt = 1;
+
+    for(int i = 1; i < n; i++){
+
+        if(arr[i] >= arr[i-1]){   // increasing consecutively
+            cnt++;
+        }
+        else{
+            cnt = 1;
+        }
+
+        ans = max(ans, cnt);
+    }
+
+    cout << ans << endl;
+}
