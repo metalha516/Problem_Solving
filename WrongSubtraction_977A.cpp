b755e5f1224aa5fd63955951df7a84f2f8f4c345
metalha516 <mehaditalha62@gmail.com>
2026-09-02 04:16:27 +0600

    Daily Practise 136

--- WrongSubtraction_977A.cpp
+++ WrongSubtraction_977A.cpp
@@ -1,15 +1,19 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
+int main()
+{
     int n, k;
-    cin>>n>>k;
-    for(int i = 0; i<k; i++){
-        if(n%10 != 0){
+    cin >> n >> k;
+    for (int i = 0; i < k; i++)
+    {
+        if (n % 10 != 0)
+        {
             n--;
-        }else{
-            n/=10;
+        }
+        else
+        {
+            n /= 10;
         }
     }
-    cout<<n<<endl;
-
+    cout << n << endl;
 }
