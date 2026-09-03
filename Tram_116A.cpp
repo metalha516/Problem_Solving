24da4869eae5d82dc1b875079015b23bbc5e03a4
metalha516 <mehaditalha62@gmail.com>
2026-09-03 14:57:54 +0600

    Daily Practise 141

--- Tram_116A.cpp
+++ Tram_116A.cpp
@@ -1,15 +1,17 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main()
+{
     int n;
-    cin>>n;
+    cin >> n;
     int exists = 0, curr = 0, max_ = 0;
-    for(int i = 0; i<n; i++){
+    for (int i = 0; i < n; i++)
+    {
         int down, up;
-        cin>>down>>up;
+        cin >> down >> up;
         exists = (exists - down) + up;
         max_ = max(max_, exists);
     }
-    cout<<max_<<endl;
+    cout << max_ << endl;
 }
