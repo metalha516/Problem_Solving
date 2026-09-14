1029c67a1ce612e7e2a6aa7aa815d5fcae75460e
metalha516 <mehaditalha62@gmail.com>
2026-09-14 21:02:42 +0600

    Daily Practise 154

---
+++ PresentFromLena_118B.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    
+    for (int i = 0; i <= n; i++) {
+        for (int j = 0; j < 2 * (n - i); j++) {
+            cout << " ";
+        }
+        for (int j = 0; j <= i; j++) {
+            cout << j << (j == 0 && i == 0 ? "" : " ");
+        }
+        for (int j = i - 1; j >= 0; j--) {
+            cout << j << (j == 0 ? "" : " ");
+        }
+        cout << "\n";
+    }
+    
+    for (int i = n - 1; i >= 0; i--) {
+        for (int j = 0; j < 2 * (n - i); j++) {
+            cout << " ";
+        }
+        for (int j = 0; j <= i; j++) {
+            cout << j << (j == 0 && i == 0 ? "" : " ");
+        }
+        for (int j = i - 1; j >= 0; j--) {
+            cout << j << (j == 0 ? "" : " ");
+        }
+        cout << "\n";
+    }
+    
+    return 0;
+}
